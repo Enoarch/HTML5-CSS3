0d23af3d85890adef25b6dec2986568de59e2a86
Thomas RENAUD <trenaud47@gmail.com>
2017-05-27 17:08:42 +0200

    Add files via upload
    Add of the jquery.min.js file and update of the js APP code (topMenu.js)

--- top nav bar -adaptive/src/topMenu.js
+++ top nav bar -adaptive/src/topMenu.js
@@ -1,1 +1,20 @@
+/* Code Javascript permettant d'adapter le menu au scroll de la sourie 
+L'utilisation du nombre flotant oldWindowPos permet de repérer dans quel sens va le scroll de la souris
+Cela évite en autre de charger tout le process pour rien et donc c'est un gain en ressource
+*/
+$(function () {
+	var myWindow = $(window);
+    var tc = $('#titleContainer')
+	var menu = $('#menu')
+	var oldWindowPos = 0;
 
+	myWindow.scroll(function () {//Au scroll sur la sourie faire...
+	   if (myWindow.scrollTop() > (tc.height()) && oldWindowPos < (tc.height()) ){
+			menu.addClass('menuFixed');
+			oldWindowPos = myWindow.scrollTop();
+		}else if(myWindow.scrollTop() < (tc.height()) && oldWindowPos > (tc.height()) ){
+	        menu.removeClass('menuFixed');
+			oldWindowPos = myWindow.scrollTop();
+		}
+	}); 
+});
